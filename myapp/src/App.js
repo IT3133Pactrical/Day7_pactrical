2760c9ac85d3ecdc3725cccb339adc3f4bfbb98f
chamithjeewantha <139670985+chamithjeewantha@users.noreply.github.com>
2024-11-21 13:31:24 +0000

    create function App and add css

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,15 @@ import Childcom from './Childcom';
 import {useState} from 'react';
 
 function App() {
-  
+  const[childname,setChildname]=useState("Default");
+  const receiveName=(name)=>{
+    setChildname(name);
+  }
+  return(
+    <div className='App'>
+
+    </div>
+  )
 }
 
 export default App;
